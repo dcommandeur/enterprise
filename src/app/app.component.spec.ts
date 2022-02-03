adfc13f9e67a6cd611d352854cd67547b659b2a5
Derek Commandeur <derek.commandeur@outlook.com>
2022-02-03 13:47:37 +0100

    Initial setup Fixes #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'd-enterprise'`, () => {
+  it(`should have as title 'enterprise'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('d-enterprise');
+    expect(app.title).toEqual('enterprise');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('d-enterprise app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('enterprise app is running!');
   });
 });
