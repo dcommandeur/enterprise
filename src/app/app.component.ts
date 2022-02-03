7ff99921ee224510ec9a9f80434cb0f25990d334
Derek Commandeur <derek.commandeur@outlook.com>
2022-02-03 13:25:57 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'djk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'd-enterprise';
+}
